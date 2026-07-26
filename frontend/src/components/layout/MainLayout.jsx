f9b0409f6abcebe76746075921ec0e9cd2b2df1c
Máximo <messinamaximo@gmail.com>
2026-07-26 20:58:51 -0300

    fix: modifica nav con links

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
-import Sidebar from './Sidebar';
 
 export default function MainLayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="flex flex-1">
-        <Sidebar />
-        <main className="p-6 flex-1 bg-white">{children}</main>
-      </div>
+      <main className="p-6 flex-1 bg-white">{children}</main>
     </div>
   );
 }
